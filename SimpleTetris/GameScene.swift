aacf23a3406f0d7b2732f864566bd0463333ec06
Sweeper777 <sumulang@gmail.com>
2016-10-02 10:32:01 +0100

    added start button

--- SimpleTetris/GameScene.swift
+++ SimpleTetris/GameScene.swift
@@ -2,7 +2,7 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    var startButton: SKSpriteNode!
     
     override func didMove(to view: SKView) {
         let banner = self.childNode(withName: "banner") as! SKSpriteNode
@@ -12,5 +12,17 @@ class GameScene: SKScene {
         let moveRight = SKAction.moveBy(x: 50, y: 0, duration: 0.7)
         let leftAndRight = SKAction.repeatForever(SKAction.sequence([moveRight, moveLeft]))
         banner.run(leftAndRight)
+        
+        startButton = self.childNode(withName: "startButton") as! SKSpriteNode
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            if startButton.frame.contains(touch.location(in: startButton)) {
+                let atlas = SKTextureAtlas(named: "startButton")
+                let animation = SKAction.animate(with: [atlas.textureNamed("startButton_p"), atlas.textureNamed("startButton")], timePerFrame: 0.2)
+                startButton.run(animation)
+            }
+        }
     }
 }
