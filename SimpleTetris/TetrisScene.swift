d6516955f59ec9c96c6f4179395fc595e22368bb
Sweeper777 <sumulang@gmail.com>
2016-10-03 07:47:49 +0100

    set layout manager

--- SimpleTetris/TetrisScene.swift
+++ SimpleTetris/TetrisScene.swift
@@ -1,7 +1,9 @@
 import SpriteKit
+import HLSpriteKit
 
 class TetrisScene: SKScene {
     override func didMove(to view: SKView) {
-        
+        let anchorPoint = NSValue(cgPoint: CGPoint(x: 0.5, y: 0.5))
+        self.hlSetLayoutManager(HLTableLayoutManager.init(columnCount: 1, columnWidths: [(0.0)], columnAnchorPoints: [anchorPoint], rowHeights: [(0.0)]))
     }
 }
