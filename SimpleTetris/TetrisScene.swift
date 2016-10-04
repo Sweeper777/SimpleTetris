f922b047a1d355ecf2781d5b117ed8eb57e37797
Sweeper777 <sumulang@gmail.com>
2016-10-04 13:27:06 +0100

    added grid view in

--- SimpleTetris/TetrisScene.swift
+++ SimpleTetris/TetrisScene.swift
@@ -3,10 +3,19 @@ import HLSpriteKit
 
 class TetrisScene: SKScene {
     var background: SKSpriteNode!
+    var tetrisGrid: HLGridNode!
     
     override func didMove(to view: SKView) {
         background = self.childNode(withName: "bg") as! SKSpriteNode
+        self.view?.ignoresSiblingOrder = true
         let anchorPoint = NSValue(cgPoint: CGPoint(x: 0.5, y: 0.5))
-        background.hlSetLayoutManager(HLTableLayoutManager.init(columnCount: 1, columnWidths: [(0.0)], columnAnchorPoints: [anchorPoint], rowHeights: [(0.0)]))
+        background.hlSetLayoutManager(HLTableLayoutManager(columnCount: 1, columnWidths: [(0.0)], columnAnchorPoints: [anchorPoint], rowHeights: [(0.0)]))
+        tetrisGrid = HLGridNode(gridWidth: 10, squareCount: 200, anchorPoint: CGPoint(x: 0.5, y: 0.5), layoutMode: .fill, squareSize: CGSize(width: 50, height: 50), backgroundBorderSize: 0, squareSeparatorSize: 0)
+        tetrisGrid.zPosition = 1000
+        tetrisGrid.backgroundColor = UIColor.black
+        tetrisGrid.squareColor = UIColor.black
+        background.addChild(tetrisGrid)
+        
+        background.hlLayoutChildren()
     }
 }
