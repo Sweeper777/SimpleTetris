9488c481e15cf6cc224077aa71e6bf22fab18470
Sweeper777 <sumulang@gmail.com>
2016-10-04 08:00:12 +0100

    Added background to tetris scene

--- SimpleTetris/TetrisScene.swift
+++ SimpleTetris/TetrisScene.swift
@@ -2,8 +2,11 @@ import SpriteKit
 import HLSpriteKit
 
 class TetrisScene: SKScene {
+    var background: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
+        background = self.childNode(withName: "bg") as! SKSpriteNode
         let anchorPoint = NSValue(cgPoint: CGPoint(x: 0.5, y: 0.5))
-        self.hlSetLayoutManager(HLTableLayoutManager.init(columnCount: 1, columnWidths: [(0.0)], columnAnchorPoints: [anchorPoint], rowHeights: [(0.0)]))
+        background.hlSetLayoutManager(HLTableLayoutManager.init(columnCount: 1, columnWidths: [(0.0)], columnAnchorPoints: [anchorPoint], rowHeights: [(0.0)]))
     }
 }
