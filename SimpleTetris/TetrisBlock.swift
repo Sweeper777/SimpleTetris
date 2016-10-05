6d60d8baef8e706be6caa02f3dcc1f9ca5460d0a
Sweeper777 <sumulang@gmail.com>
2016-10-05 07:20:45 +0100

    partially done TetrisBlock

--- SimpleTetris/TetrisBlock.swift
+++ SimpleTetris/TetrisBlock.swift
@@ -2,7 +2,37 @@ import SpriteKit
 
 public class TetrisBlock {
     let node: SKSpriteNode
-    let color: SKColor
-    var x: Int
-    var y: Int
+    var texture: SKTexture {
+        didSet {
+            node.texture = texture
+        }
+    }
+    
+    weak var tetrisBoard: TetrisBoard?
+    
+    var x: Int {
+        didSet {
+            
+        }
+    }
+    
+    var y: Int {
+        didSet {
+            
+        }
+    }
+    
+    func moveDown() {
+        y -= 1
+    }
+    
+    init(x: Int, y: Int, texture: SKTexture, tetrisBoard: TetrisBoard) {
+        self.x = x
+        self.y = y
+        self.texture = texture
+        self.tetrisBoard = tetrisBoard
+        self.node = SKSpriteNode(texture: texture)
+        self.node.size = CGSize(width: 50, height: 50)
+        self.tetrisBoard!.tetrisBlocks[x][y] = self
+    }
 }
