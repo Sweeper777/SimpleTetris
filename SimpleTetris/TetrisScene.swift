fffebacd5df086300c126f35ae20a98123610e6e
Sweeper777 <sumulang@gmail.com>
2016-10-08 11:44:13 +0100

    added row separator of table layout

--- SimpleTetris/TetrisScene.swift
+++ SimpleTetris/TetrisScene.swift
@@ -11,7 +11,9 @@ class TetrisScene: SKScene {
         background = self.childNode(withName: "bg") as! SKSpriteNode
         self.view?.ignoresSiblingOrder = true
         let anchorPoint = NSValue(cgPoint: CGPoint(x: 0.5, y: 0.5))
-        background.hlSetLayoutManager(HLTableLayoutManager(columnCount: 1, columnWidths: [(0.0)], columnAnchorPoints: [anchorPoint], rowHeights: [(0.0)]))
+        let tableLayout = HLTableLayoutManager(columnCount: 1, columnWidths: [(0.0)], columnAnchorPoints: [anchorPoint], rowHeights: [(0.0)])!
+        tableLayout.rowSeparator = 15
+        background.hlSetLayoutManager(tableLayout)
         tetrisGrid = HLGridNode(gridWidth: 10, squareCount: 200, anchorPoint: CGPoint(x: 0.5, y: 0.5), layoutMode: .fill, squareSize: CGSize(width: 50, height: 50), backgroundBorderSize: 0, squareSeparatorSize: 0)
         tetrisGrid.zPosition = 1000
         tetrisGrid.backgroundColor = UIColor.black
