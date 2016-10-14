727d22fce85e2bb2030606f205912a3479914ee5
Sweeper777 <sumulang@gmail.com>
2016-10-14 07:12:53 +0100

    enhancement to ButtonNode

--- SimpleTetris/ButtonNode.swift
+++ SimpleTetris/ButtonNode.swift
@@ -4,6 +4,7 @@ class ButtonNode: SKSpriteNode {
     var onClick: (() -> Void)?
     var unpressedTexture: SKTexture?
     var pressedTexture: SKTexture?
+    var callBackFirst = true
     
     func onTouch() {
         let action = SKAction.animate(with: [pressedTexture!, unpressedTexture!], timePerFrame: 0.1)
@@ -12,7 +13,12 @@ class ButtonNode: SKSpriteNode {
             self?.onClick?()
         }
         
-        let sequence = SKAction.sequence([callBack, action])
-        self.run(sequence)
+        if callBackFirst {
+            let sequence = SKAction.sequence([callBack, action])
+            self.run(sequence)
+        } else {
+            let sequence = SKAction.sequence([action, callBack])
+            self.run(sequence)
+        }
     }
 }
